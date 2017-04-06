8c6ac15b75a8cd38e3a581cf54ae370376190919
Victor <iaw47865910@h13.informatica.escoladeltreball.org>
2017-04-06 21:10:46 +0200

    Optimize code

--- app.js
+++ app.js
@@ -2,20 +2,15 @@ var express = require("express"),
     bodyParser = require('body-parser'),
     http = require('http');
 
-//Instanciem l'objecte express
+// We instantiate express and other modules
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-//optional , el motor de renderitzar per defecte es jade
-//app.engine('jade', require('jade').__express);
 app.set('view engine', 'jade');
 
 require("./controllers/routes")(app);
 require("./.env");
-console.log(ENVIRONMENT);
-console.log("HOLA");
 
 if(ENVIRONMENT == "TESTING"){
     app.listen(8080, function(){
